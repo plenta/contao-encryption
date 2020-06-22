5cce87f65c4bd4f121a199b9f0672271163db044
Christoph Werner <christopwerner@gmx.at>
2020-06-22 14:31:59 +0200

    PHPUnit

---
+++ src/Brkwsky/ContaoEncryptionBundle/Tests/Unit/ContaoEncryptionBundle/ContaoEncryptionBundleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Encryption extension for Contao Open Source CMS
+ *
+ * @copyright     Copyright (c) 2020, Christian Barkowsky & Christoph Werner
+ * @author        Christian Barkowsky <https://brkwsky.de/>
+ * @author        Christoph Werner <https://brkwsky.de/>
+ */
+
+namespace Brkwsky\ContaoEncryptionBundle\Tests\Unit\ContaoEncryptionBundle;
+
+use Brkwsky\ContaoEncryptionBundle\ContaoEncryptionBundle;
+use PHPUnit\Framework\TestCase;
+
+class ContaoEncryptionBundleTest extends TestCase
+{
+    public function testCanBeInstantiated(): void
+    {
+        $bundle = new ContaoEncryptionBundle();
+        $this->assertInstanceOf('Brkwsky\ContaoEncryptionBundle\ContaoEncryptionBundle', $bundle);
+    }
+}
