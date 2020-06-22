060ca203344668083f83d32eedbda21ff00ed594
Christoph Werner <christopwerner@gmx.at>
2020-06-22 14:54:06 +0200

    ...

--- src/Brkwsky/ContaoEncryptionBundle/ContaoManager/Plugin.php
+++ src/Brkwsky/ContaoEncryptionBundle/ContaoManager/Plugin.php
@@ -17,7 +17,6 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Symfony\Component\Config\Loader\LoaderInterface;
 
 class Plugin implements BundlePluginInterface
 {
